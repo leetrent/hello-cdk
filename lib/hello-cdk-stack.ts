45278ab3c03eb09e683cfe4949fa0fb1a49f78d7
leetrent <10787590+leetrent@users.noreply.github.com>
2022-11-16 07:05:56 -0600

    Dev (#1)
    * Added vs to git ignore
    
    * Added SQS Queue
    
    * Added s3 bucket

--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -1,6 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
+import { RemovalPolicy } from 'aws-cdk-lib';
 
 export class HelloCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -8,9 +10,16 @@ export class HelloCdkStack extends cdk.Stack {
 
     // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'HelloCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Simple Queue Service (SQS)
+    const queue = new sqs.Queue(this, 'HelloCdkQueue', {
+      visibilityTimeout: cdk.Duration.seconds(300)
+    });
+
+    // S3 Bucket
+    const bucket = new Bucket(this, "HelloCdkBucket", {
+      versioned: true,
+      removalPolicy: RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
   }
 }
